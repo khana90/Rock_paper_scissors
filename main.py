f6938a48c2bde4d9ee00f91fa2b9bd30de2a50e0
A khan <awal.khan@live.co.uk>
2024-07-03 02:22:25 +0000

    updated pyton

--- main.py
+++ main.py
@@ -31,10 +31,10 @@ user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 f
 if user_choice >= 3 or user_choice < 0:
     print("You type invalide number, you lose!")
 else:
-        print(game_images[user_choice])
-        computer_choice = random.randint(0,2)
-        print ("computer chose:")
-        print(game_images[computer_choice])
+    print(game_images[user_choice])
+    computer_choice = random.randint(0,2)
+    print ("computer chose:")
+    print(game_images[computer_choice])   
     if user_choice == 0 and computer_choice == 2:
         print ("You Win !")
     elif computer_choice == 0 and user_choice == 2:
@@ -44,4 +44,4 @@ else:
     elif user_choice > computer_choice:
         print("You Win!")
     elif computer_choice == user_choice:
-        print("Its a draw")
+            print("Its a draw")
